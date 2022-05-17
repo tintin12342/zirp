79070058f1803ed40ff84f271f9ceef68806a54f
Tin <tin.vaiti5@gmail.com>
2022-05-17 19:00:43 +0200

    changed neighbors to 10 and implemented the elbow method

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
-from sklearn.metrics import classification_report
 import pandas as pd
+import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import classification_report, confusion_matrix
+from sklearn.cluster import KMeans
+import matplotlib.pyplot as plt
+
+
+def write_results(title, text):
+    with open('results.txt', 'w') as f:
+        f.write(title + '\n' + '\n')
+        f.writelines('\n'.join(text))
 
 
 class DataPreprocessing:
@@ -14,9 +23,14 @@ class DataPreprocessing:
         use_cols = ['Model', 'Vehicle Class', 'Engine Size', 'Cylinders', 'Transmission',
                     'Fuel', 'Fuel Consumption', 'Unnamed: 9', 'Unnamed: 10', 'CO2']
         df = pd.read_csv(self.file_path, sep=',', encoding='cp1252', low_memory=False, usecols=use_cols)
-        df = df.rename(columns={'Unnamed: 9': 'Hwy Fuel Consumption', 'Unnamed: 10': 'Comb Fuel Consumption'})
+
+        df = df.rename(columns={'Unnamed: 9': 'Hwy Fuel Consumption',
+                                'Unnamed: 10': 'Comb Fuel Consumption',
+                                'CO2': 'CO2 Ratings'})
         df = df.dropna()
+
         self._factorize_data(df, ['Vehicle Class', 'Transmission', 'Fuel'])
+
         self.preprocessed_data = df
 
     def get_preprocessed_data(self):
@@ -35,20 +49,43 @@ class KNN:
         self.data = data
 
     def train_data(self):
-        X = self.data.loc[:, self.data.columns != 'CO2']
-        y = self.data['CO2']
+        X = self.data.loc[:, self.data.columns != 'CO2 Ratings']
+        y = self.data['CO2 Ratings']
+
         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20)
-        knn = KNeighborsClassifier(n_neighbors=3)
+
+        knn = KNeighborsClassifier(n_neighbors=10)
         knn.fit(X_train, y_train)
-        prediction_results = knn.predict(X)
-        self.data.insert(len(self.data.columns), "Prediction Results", prediction_results)
+
+        y_pred = knn.predict(X_test)
+        write_results('KNN', [str(confusion_matrix(y_test, y_pred, labels=np.unique(y_pred))),
+                              str(classification_report(y_test, y_pred, labels=np.unique(y_pred)))])
+
+        self.data.insert(len(self.data.columns), "Prediction Results", knn.predict(X))
+
         return self.data
+
+    def elbow_method(self):
+        distortions = []
+        K = range(1, 10)
+        for k in K:
+            kmean_model = KMeans(n_clusters=k)
+            kmean_model.fit(self.data)
+            distortions.append(kmean_model.inertia_)
+
+        plt.figure(figsize=(16, 8))
+        plt.plot(K, distortions, 'bx-')
+        plt.xlabel('k')
+        plt.ylabel('Distortion')
+        plt.title('The Elbow Method showing the optimal k')
+        plt.show()
 
 
 if __name__ == '__main__':
     try:
         data = DataPreprocessing(r'C:\Users\tinva\Desktop\MY2022.csv')
         knn = KNN(data.get_preprocessed_data())
-        print(knn.train_data())
+        knn.train_data()
+        knn.elbow_method()
     except:
         print('Error while loading data')
